7b47e5fe55d638efeb78b4b3fd7c5daf1a33088e
hwq <weiqi.huang@pisofttech.com>
2018-05-08 17:46:12 +0800

    整理代码

---
+++ app/src/main/java/com/abt/adaptation/AdaptationApp.java
@@ -0,0 +1,19 @@
+package com.abt.adaptation;
+
+import android.app.Application;
+
+/**
+ * @描述： @AdaptationApp
+ * @作者： @黄卫旗
+ * @创建时间： @2018/5/8
+ */
+public class AdaptationApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+}

1778e03e5eccb33640e0bfe4ee4bb22278cff404
huangweiqi <askviky2010@gmail.com>
2018-05-10 00:34:27 +0800

    整理获取屏幕信息的接口

--- app/src/main/java/com/abt/adaptation/MainActivity.java
+++ app/src/main/java/com/abt/adaptation/MainActivity.java
@@ -1,9 +1,8 @@
 package com.abt.adaptation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.util.Log;
 
 import com.orhanobut.logger.Logger;
 
@@ -13,29 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         // 800x480 - Inch4.0
         // 1280x720 - Inch4.7
+        getScreenInfo();
+        getDeviceInch();
+    }
 
-
+    public void getScreenInfo() {
         DisplayMetrics metric = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metric);
         int width =metric.widthPixels;  //宽度（PX）
         int height =metric.heightPixels;  //高度（PX）
         float density =metric.density;  //密度（0.75 / 1.0 / 1.5）
         int densityDpi =metric.densityDpi;  //密度DPI（120 / 160 / 240）
-        Logger.d("宽度（PX）= "+width);
-        Logger.d("高度（PX）= "+height);
-        Logger.d("密度 = "+density);
-        Logger.d("密度DPI = "+densityDpi);
-
-        getScreenInch();
+        Logger.d("宽度（PX）= "+width+", 高度（PX）= "+height+", 密度 = "+density+", 密度DPI = "+densityDpi);
     }
 
-
     // 获取设备的物理尺寸
-    public void getScreenInch() {
+    public void getDeviceInch() {
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         double x = Math.pow(dm.widthPixels/dm.xdpi,2);

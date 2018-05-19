ddcafc6aa0f8691cbf362be56694658f23616593
huangweiqi <askviky2010@gmail.com>
2018-05-20 02:19:23 +0800

    修正dimenTools代码

--- adapter/src/main/java/DimenTool.java
+++ adapter/src/main/java/DimenTool.java
@@ -20,21 +20,29 @@ public class DimenTool {
     public static void generateDimens() {
         String filePre = "/Users/huangweiqi/StudioProjects/MultiScreenAdaptation/adapter/";
         String w240file = filePre+"src/main/res/values-w240dp/dimens.xml";
+        String w270file = filePre+"src/main/res/values-w270dp/dimens.xml";
+        String w320file = filePre+"src/main/res/values-w320dp/dimens.xml";
+        String w360file = filePre+"src/main/res/values-w360dp/dimens.xml";
         String w480file = filePre+"src/main/res/values-w480dp/dimens.xml";
-        String sw600file = filePre+"src/main/res/values-w600dp/dimens.xml";
-        String sw720file = filePre+"src/main/res/values-w720dp/dimens.xml";
-        String sw800file = filePre+"src/main/res/values-w800dp/dimens.xml";
-        String sw820file = filePre+"src/main/res/values-w820dp/dimens.xml";
+        String w540file = filePre+"src/main/res/values-w540dp/dimens.xml";
+        String w600file = filePre+"src/main/res/values-w600dp/dimens.xml";
+        String w720file = filePre+"src/main/res/values-w720dp/dimens.xml";
+        String w800file = filePre+"src/main/res/values-w800dp/dimens.xml";
+        String w820file = filePre+"src/main/res/values-w820dp/dimens.xml";
 
         // 以此文件夹下的dimens.xml文件内容为初始值参照
         File file = new File(filePre+"src/main/res/values/dimens.xml");
         BufferedReader reader = null;
-        StringBuilder sw240 = new StringBuilder();
+        StringBuilder w240 = new StringBuilder();
+        StringBuilder w270 = new StringBuilder();
+        StringBuilder w320 = new StringBuilder();
+        StringBuilder w360 = new StringBuilder();
         StringBuilder w480 = new StringBuilder();
-        StringBuilder sw600 = new StringBuilder();
-        StringBuilder sw720 = new StringBuilder();
-        StringBuilder sw800 = new StringBuilder();
-        StringBuilder sw820 = new StringBuilder();
+        StringBuilder w540 = new StringBuilder();
+        StringBuilder w600 = new StringBuilder();
+        StringBuilder w720 = new StringBuilder();
+        StringBuilder w800 = new StringBuilder();
+        StringBuilder w820 = new StringBuilder();
 
         try {
             System.out.println("生成不同分辨率：");
@@ -57,42 +65,64 @@ public class DimenTool {
                                     tempString.indexOf("</dimen>") - 2));
 
                     //根据不同的尺寸，计算新的值，拼接新的字符串，并且结尾处换行。
-                    sw240.append(start).append(num * 0.75).append(end).append("\r\n");
+                    w240.append(start).append(num * 240/240).append(end).append("\r\n");
+                    w270.append(start).append(num * 270/240).append(end).append("\r\n");
+                    w320.append(start).append(num * 320/240).append(end).append("\r\n");
+                    w360.append(start).append(num * 360/240).append(end).append("\r\n");
                     w480.append(start).append(num * 480/240).append(end).append("\r\n");
-                    sw600.append(start).append(num * 1.87).append(end).append("\r\n");
-                    sw720.append(start).append(num * 2.25).append(end).append("\r\n");
-                    sw800.append(start).append(num * 2.5).append(end).append("\r\n");
-                    sw820.append(start).append(num * 2.56).append(end).append("\r\n");
+                    w540.append(start).append(num * 540/240).append(end).append("\r\n");
+                    w600.append(start).append(num * 600/240).append(end).append("\r\n");
+                    w720.append(start).append(num * 720/240).append(end).append("\r\n");
+                    w800.append(start).append(num * 800/240).append(end).append("\r\n");
+                    w820.append(start).append(num * 820/240).append(end).append("\r\n");
                 } else {
-                    sw240.append(tempString).append("\r\n");
+                    w240.append(tempString).append("\r\n");
+                    w270.append(tempString).append("\r\n");
+                    w320.append(tempString).append("\r\n");
+                    w360.append(tempString).append("\r\n");
                     w480.append(tempString).append("\r\n");
-                    sw600.append(tempString).append("\r\n");
-                    sw720.append(tempString).append("\r\n");
-                    sw800.append(tempString).append("\r\n");
-                    sw820.append(tempString).append("\r\n");
+                    w540.append(tempString).append("\r\n");
+                    w600.append(tempString).append("\r\n");
+                    w720.append(tempString).append("\r\n");
+                    w800.append(tempString).append("\r\n");
+                    w820.append(tempString).append("\r\n");
                 }
                 line++;
             }
 
             reader.close();
-            System.out.println("<!--  sw240 -->");
-            System.out.println(sw240);
+            System.out.println("<!--  w240 -->");
+            System.out.println(w240);
+            System.out.println("<!--  w270 -->");
+            System.out.println(w270);
+            System.out.println("<!--  w320 -->");
+            System.out.println(w320);
+            System.out.println("<!--  w360 -->");
+            System.out.println(w360);
             System.out.println("<!--  w480 -->");
             System.out.println(w480);
-            System.out.println("<!--  sw600 -->");
-            System.out.println(sw600);
-            System.out.println("<!--  sw720 -->");
-            System.out.println(sw720);
-            System.out.println("<!--  sw800 -->");
-            System.out.println(sw800);
+            System.out.println("<!--  w540 -->");
+            System.out.println(w540);
+            System.out.println("<!--  w600 -->");
+            System.out.println(w600);
+            System.out.println("<!--  w720 -->");
+            System.out.println(w720);
+            System.out.println("<!--  w800 -->");
+            System.out.println(w800);
+            System.out.println("<!--  w820 -->");
+            System.out.println(w820);
 
             //将新的内容，写入到指定的文件中去
-            //writeFile(sw240file, sw240.toString());
-            writeFile(w480file, w480.toString());
-            //writeFile(sw600file, sw600.toString());
-            //writeFile(sw720file, sw720.toString());
-            //writeFile(sw800file, sw800.toString());
-            //writeFile(sw820file, sw820.toString());
+            //writeFile(w240file, w240.toString());
+            writeFile(w270file, w270.toString());
+            //writeFile(w320file, w320.toString());
+            //writeFile(w360file, w360.toString());
+            //writeFile(w480file, w480.toString());
+            writeFile(w540file, w540.toString());
+            writeFile(w600file, w600.toString());
+            writeFile(w720file, w720.toString());
+            writeFile(w800file, w800.toString());
+            writeFile(w820file, w820.toString());
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
